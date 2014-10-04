a6a22e94a4a535f2e2f7fd2de836b360f0e10820
Aleksey Kuznetsov <zummenix@gmail.com>
2014-10-04 13:30:15 +0600

    Added a 'main.rs' file as example.

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+
+extern crate "xor-rs" as xorfn;
+extern crate collections;
+
+fn main() {
+    let source = [95, 80, 96, 71, 120, 25, 44, 92, 120, 71, 96, 79, 54];
+    let result = xorfn::xor(source, [23, 53, 12, 43]);
+
+    match collections::str::from_utf8(result.as_slice()) {
+        Some(string) => println!("{}", string),
+        _ => {}
+    }
+}
