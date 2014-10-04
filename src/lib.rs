fe5571224076d2ecfd0613cf06cfa6b24778bc66
Aleksey Kuznetsov <zummenix@gmail.com>
2014-10-04 22:27:15 +0600

    Rewritten tests to use a 'stainless' testing framework.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
 
+#![feature(phase)]
+#[phase(plugin, link)]
+extern crate stainless;
+
 pub fn xor(source: &[u8], key: &[u8]) -> Vec<u8> {
     if source.len() == 0 || key.len() == 0 {
         return source.to_vec();
@@ -39,27 +43,20 @@ fn next_index(current: uint, count: uint) -> uint {
     }
 }
 
-#[cfg(test)]
-mod test {
-
-    use super::xor;
-
-    #[test]
-    fn test_result() {
+describe! test_xor {
+    it "should return right result" {
         let source = [0, 1, 2, 3];
         let key = [34, 52];
         assert!(xor(source, key).as_slice() == [34, 53, 32, 55]);
     }
 
-    #[test]
-    fn test_with_empty_key() {
+    it "should return source if key is empty" {
         let source = [0, 1, 2, 3];
         let key = [];
         assert!(xor(source, key).as_slice() == [0, 1, 2, 3]);
     }
 
-    #[test]
-    fn test_with_empty_source() {
+    it "should return empty result if source is empty" {
         let source = [];
         let key = [45, 32, 56];
         assert!(xor(source, key).as_slice() == []);
