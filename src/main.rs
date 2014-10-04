1ca69358d409243aa7eb5f84af4b27cf4c667886
Aleksey Kuznetsov <zummenix@gmail.com>
2014-10-04 19:13:36 +0600

    Changed name of the lib.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 
-extern crate "xor-rs" as xorfn;
+extern crate "xor_rs" as xor;
 extern crate collections;
 
 #[cfg(not(test))]
 fn main() {
     let source = [95, 80, 96, 71, 120, 25, 44, 92, 120, 71, 96, 79, 54];
-    let result = xorfn::xor(source, [23, 53, 12, 43]);
+    let result = xor::xor(source, [23, 53, 12, 43]);
 
     match collections::str::from_utf8(result.as_slice()) {
         Some(string) => println!("{}", string),
