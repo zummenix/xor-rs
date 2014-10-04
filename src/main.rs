4c7c439643d8ffde839899a8ef963114553a9d43
Aleksey Kuznetsov <zummenix@gmail.com>
2014-10-04 14:32:27 +0600

    A 'main' function excluded from tests.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate "xor-rs" as xorfn;
 extern crate collections;
 
+#[cfg(not(test))]
 fn main() {
     let source = [95, 80, 96, 71, 120, 25, 44, 92, 120, 71, 96, 79, 54];
     let result = xorfn::xor(source, [23, 53, 12, 43]);
