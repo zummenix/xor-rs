84bb662b127a87845abadc908efa1de035064f78
Aleksey Kuznetsov <zummenix@gmail.com>
2014-10-05 15:13:36 +0600

    An example moved to correct place.

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -2,7 +2,6 @@
 extern crate "xor_rs" as xor;
 extern crate collections;
 
-#[cfg(not(test))]
 fn main() {
     let source = [95, 80, 96, 71, 120, 25, 44, 92, 120, 71, 96, 79, 54];
     let result = xor::xor(source, [23, 53, 12, 43]);
