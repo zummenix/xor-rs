8d9da42d7a51244f0079291217f3b1074ac4f0ec
Aleksey Kuznetsov <zummenix@gmail.com>
2014-12-24 22:58:03 +0500

    Upgraded to latest Rust.

--- examples/example.rs
+++ examples/example.rs
@@ -6,7 +6,7 @@ fn main() {
     let source = &[95, 80, 96, 71, 120, 25, 44, 92, 120, 71, 96, 79, 54];
     let result = xor::xor(source, &[23, 53, 12, 43]);
 
-    if let Some(string) = collections::str::from_utf8(result.as_slice()) {
-        println!("{}", string)
+    if let Ok(string) = collections::str::from_utf8(result.as_slice()) {
+        println!("{}", string);
     }
 }
